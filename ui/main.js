18e9f5f12077aef3b1f7db5c9c06fab6d1e31ee4
Sarvesh Singh <sarvesh.singh18@hotmail.com>
2016-10-10 23:28:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,9 +2,19 @@ console.log("Loaded!");
 
 alert('Loaded Successfully!');
 
-var element=document.getElementById("main-text");
-element.innerHTML="Sarvesh";
+var element=document.getElementById('main-text');
+element.innerHTML='Sarvesh';
 var image=document.getElementById('madi');
 image.onclick=function() {
-image.style.marginLeft='100px';
+image.style.marginLeft="100px";
 };
+
+/*
+var marginLeft=0;
+function moveRight() {
+marginLeft=marginLeft + '1';
+image.style.maginLeft=marginLeft+'px';
+var image=document.getElementById('madi');
+img.onclick=function() {
+var interval=setInterval(moveRight,50);//50 in ms
+};*/
