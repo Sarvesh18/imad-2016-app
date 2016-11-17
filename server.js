2bd8ed15ba6b02e158d06bff4a62fe317b1f96ee
Sarvesh Singh <sarvesh.singh18@hotmail.com>
2016-11-18 05:17:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,19 +2,89 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var Pool = require('pg').Pool;
+var crypto = require('crypto');
+var bodyParser = require('body-parser');
+
+
+
 var app = express();
 app.use(morgan('combined'));
 
-/*
-app.get('/:articleName', function (req, res) {
- 
-//Express Framework after : take in var  
-//articles[articleName]=={} content object for article one
-var articleName=req.params.articleName;       
-res.send(createTemplate(articles[articleName]));
+app.use(bodyParser.json());
 
+
+
+var config = {
+  host: 'db.imad.hasura-app.io',
+  port: '5432',
+  user: 'sarvesh18',
+  //password: 'db-sarvesh18-95101',
+  database: 'sarvesh18',
+  password: process.env.DB_PASSWORD 
+};
+
+
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-*/
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+function hash(input, salt) {
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$'); 
+}
+
+app.get('/hash/:input', function (req, res) {
+    var hashedString = hash(req.params.input,'salt');       
+	res.send(hashedString);
+});
+
+//Post Request
+app.post('/signup', function (req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+	var salt = crypto.randomBytes(128).toString('hex');       
+	var dbString = hash(password, salt);
+    pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function(err, result) {
+    if(err) {
+        res.status(500).send(err.toString());
+    } 
+    else {
+        res.send('User Successfully Created:'+username);
+    } 
+  });
+});
+
+//Post Request
+app.post('/login', function (req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    pool.query('SELECT * FROM "user" WHERE username = $1', [username], function(err, result) {
+    if(err) {
+        res.status(500).send(err.toString());
+    } 
+    else {
+        if(result.rows.length == 0) {
+            res.send(403).send('Username/Password is Invalid');
+        }
+        else {
+           var dbString = result.rows[0].password;
+           var salt = dbString.split('$')[2];
+           var hashedPassword  = hash(password, salt);
+           if(hashedPassword == dbString) {
+            res.send('Credentials Correct!');
+            //Set a Session
+           }
+           else {
+            res.send(403).send('Username/Password is Invalid');
+           } 
+          }
+         }
+        });
+});
+
 
 var articles = {
 'article-one': {
@@ -104,29 +174,12 @@ var htmlTemplate=`
 return htmlTemplate;
 }
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-var counter = 0;
-app.get('/counter',function(req, res) {
-    counter = counter + 1;
-    res.send(counter.toString());
-});
-
-var names = [];
-app.get('/submit-name',function(req, res) { //url: /submit-name?name=xxxxx
- //Get Name From Request
- var name = req.query.name;
- names.push(name);
- //JSON:Javascript Object Notation
- res.send(JSON.stringify(names));
-});
-
 app.get('/:articleName', function (req, res) {
 var articleName=req.params.articleName;       
 res.send(createTemplate(articles[articleName]));
 });
+
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
@@ -146,6 +199,22 @@ app.listen(8080, function () {
 });
 
 
+var pool = new Pool(config);
 
+app.get('/:test-db', function (req, res) {
+
+	//make a select request
+        //return a response with the results
+	pool.query('SELECT * FROM test',function(err, result) {
+         if(err) {
+          res.status(500).send(err.toString());
+         } else {
+          res.send(JSON.stringify(result));
+          res.send(JSON.stringify(result.rows));//Only Rows
+         }
+	});
+
+
+});
 
 
