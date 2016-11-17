a167d72e08406bc7d731cdd853dd7d5e4d188523
Sarvesh Singh <sarvesh.singh18@hotmail.com>
2016-11-18 04:09:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -57,3 +57,32 @@ button.onclick = function() {
  request.open('GET', 'http://sarvesh18.imad.hasura-app.io/counter', true);
  request.send(null);
 };
+
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+ //Create Request Object
+ var request = new XMLHttpRequest();
+ 
+ //Capture Response & Store in Var
+ request.onreadystatechange = function() {
+  if(request.readyState == XMLHttpRequest.DONE) {
+   //Take Some Action
+   if(request.status == 200) { 
+    //Capture List & Render
+    var names = request.responseText;
+    names = JSON.parse(names);
+    var list = '';
+    for(var i=0; i<names.length; i++) {
+     list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+   }
+  }
+ };
+ var nameInput = document.getElementById('name');
+ var name = nameInput.value;
+ //Make a Request
+ request.open('GET', 'http://sarvesh18.imad.hasura-app.io/submit-name?name=' +  name, true);
+ request.send(null);
+};
