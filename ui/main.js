beaf91faf757275d77720f16438dcbbeb46732fa
Sarvesh Singh <sarvesh.singh18@hotmail.com>
2016-11-18 02:20:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,14 @@ console.log("Loaded!");
 
 alert('Loaded Successfully!');
 
+var button = document.getElementById('counter');
+var counter=0;
+
+button.onclick=function() {
+counter= counter+1;
+var span=document.getElementById('count');
+span.innerHTML=counter.toString();
+};
 /*
 var element=document.getElementById('main-text');
 element.innerHTML='Sarvesh';
