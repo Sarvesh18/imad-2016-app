a569359031d7e7fada23c7e09a29a2a3e2bf7d33
Sarvesh Singh <sarvesh.singh18@hotmail.com>
2016-11-18 02:48:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,7 +23,7 @@ image.onclick=function() {
 var interval=setInterval(moveRight,50);//50 in ms
 };
 
-/*
+
 var button = document.getElementById('counter');
 var counter=0;
 
@@ -32,27 +32,4 @@ counter= counter+1;
 var span=document.getElementById('count');
 span.innerHTML=counter.toString();
 };
-*/
 
-//AJAX
-var button = document.getElementById('counter');
-
-button.onclick=function() {
- //Request Object
- var request = new XMLHttpRequest();
-
- //Request Stage Browser Understand: 1-Open 2-Send 3-Loading 4-Loaded Successfully
- 
- request.onreadystatechange = function() {
-  if(request.readyState == XMLhttpRequest.DONE) {
-   if(request.state == 200) { 
-    var counter = request.responseText;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
-   }
-  }
- };
- //Make a request to the counter endpoint
- request.open('GET', 'http://sarvesh18.imad.hasura-app.io/counter', true);
- request.send(null);
-};
