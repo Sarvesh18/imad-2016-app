c658bf87b8f89603be0e7a9b330c4d11c5c93761
Sarvesh Singh <sarvesh.singh18@hotmail.com>
2016-11-18 05:17:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,63 +1,3 @@
-//console.log("Loaded!");
-
-//alert('Loaded Successfully!');
-
-/*
-var element=document.getElementById('main-text');
-element.innerHTML='Sarvesh';
-var image=document.getElementById("madi");
-image.onclick=function() {
-image.style.marginLeft="100px";
-};
-*/
-/*
-var element=document.getElementById('main-text');
-element.innerHTML='Sarvesh';
-var image=document.getElementById('madi');
-var marginLeft=0;
-function moveRight() {
-marginLeft=marginLeft + 1;
-image.style.marginLeft=marginLeft+'px';
-}
-image.onclick=function() {
-var interval=setInterval(moveRight,50);//50 in ms
-};
-*/
-/*
-var button = document.getElementById('counter');
-var counter=0;
-
-button.onclick=function() {
-counter= counter+1;
-var span=document.getElementById('count');
-span.innerHTML=counter.toString();
-};
-*/
-
-var button = document.getElementById('counter');
-
-button.onclick = function() {
- //Request Object
- var request = new XMLHttpRequest();
-
- //Request Stage Browser Understand: 1-Open 2-Send 3-Loading 4-Loaded Successfully
- 
- request.onreadystatechange = function() {
-  if(request.readyState == XMLHttpRequest.DONE) {
-   if(request.status == 200) { 
-    var counter = request.responseText;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
-   }
-  }
-  else {
-  }
- };
- //Make a request to the counter endpoint
- request.open('GET', 'http://sarvesh18.imad.hasura-app.io/counter', true);
- request.send(null);
-};
-
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
  //Create Request Object
@@ -68,21 +8,23 @@ submit.onclick = function() {
   if(request.readyState == XMLHttpRequest.DONE) {
    //Take Some Action
    if(request.status == 200) { 
-    //Capture List & Render
-    var names = request.responseText;
-    names = JSON.parse(names);
-    var list = '';
-    for(var i=0; i<names.length; i++) {
-     list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    alert('Logged In Successfully');
+   }
+   else if(request.status == 403) { 
+    alert('username/password is incorrect');
+   }
+   else if(request.status == 500) { 
+    alert('Something wen Wrong!!!');
    }
   }
  };
- var nameInput = document.getElementById('name');
- var name = nameInput.value;
+
+ var username = document.getElementById('username').value;
+ var password = document.getElementById('password').value;
+ console.log(username);
+ console.log(password);
  //Make a Request
- request.open('GET', 'http://sarvesh18.imad.hasura-app.io/submit-name?name=' +  name, true);
- request.send(null);
+ request.open('POST', 'http://sarvesh18.imad.hasura-app.io/login', true);
+ request.send(JSON, stringify({username: username, password:password}));
 };
+
