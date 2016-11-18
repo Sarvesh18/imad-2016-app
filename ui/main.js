5591074a2ee4458ae7b31b7b9af1ed94fac8b64f
Sarvesh Singh <sarvesh.singh18@hotmail.com>
2016-11-19 02:06:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,34 @@
-var submit = document.getElementById('submit_btn');
+var submit = document.getElementById('submit_btn_login');
+submit.onclick = function() {
+ //Create Request Object
+ var request = new XMLHttpRequest();
+ 
+ //Capture Response & Store in Var
+ request.onreadystatechange = function() {
+  if(request.readyState == XMLHttpRequest.DONE) {
+   //Take Some Action
+   if(request.status == 200) { 
+    alert('Logged In Successfully');
+   }
+   else if(request.status == 403) { 
+    alert('username/password is incorrect');
+   }
+   else if(request.status == 500) { 
+    alert('Something wen Wrong!!!');
+   }
+  }
+ };
+ var username = document.getElementById('username').value;
+ var password = document.getElementById('password').value;
+ console.log(username);
+ console.log(password);
+ //Make a Request
+ request.open('POST', 'http://sarvesh18.imad.hasura-app.io/login', true);
+ request.setRequestHeader('Content-Type','application/json');
+ request.send(JSON.stringify({username: username, password:password}));
+};
+
+var submit = document.getElementById('submit_btn_signup');
 submit.onclick = function() {
  //Create Request Object
  var request = new XMLHttpRequest();
