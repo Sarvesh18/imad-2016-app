8ff53882f8d6749eb843f7f2774d232b0a57f4b1
Sarvesh Singh <sarvesh.singh18@hotmail.com>
2016-11-19 21:37:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -225,7 +225,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/http://www.jumboedusupplies.com/images/favicon.ico', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
