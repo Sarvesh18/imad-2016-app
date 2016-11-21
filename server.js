3cf5ecda3db81f77df046d68a82a078ab843fa55
Sarvesh Singh <sarvesh.singh18@hotmail.com>
2016-11-21 22:35:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ var config = {
   port: '5432', //password: 'db-sarvesh18-95101',
   password: process.env.DB_PASSWORD //Environment Variable
 };
-var pool = new Pool(config);
 ////////////////////////////////////////////////////////////////////////////////
 var app = express();
 app.use(morgan('combined'));
@@ -29,6 +28,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 ////////////////////////////////////////////////////////////////////////////////
+var pool = new Pool(config);
 function hash(input, salt) {
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$'); 
@@ -127,6 +127,9 @@ app.get('/logout', function (req, res) {
 
 });
 
+/*
+var pool = new Pool(config);
+
 app.get('/:test-db', function (req, res) {
 
 	//make a select request
@@ -140,8 +143,6 @@ app.get('/:test-db', function (req, res) {
          }
 	});
 });
-/*
-var pool = new Pool(config);
 
 app.get('/get-articles', function (req, res) {
    // make a select request
