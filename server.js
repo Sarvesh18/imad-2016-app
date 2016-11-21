76ffc25930144d56e0691f02f19418d5580f54a8
Sarvesh Singh <sarvesh.singh18@hotmail.com>
2016-11-21 22:11:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 ////////////////////////////////////////////////////////////////////////////////
 var config = {
-  host: 'localhost',
-  //host: 'db.imad.hasura-app.io',
+  host: 'db.imad.hasura-app.io',
   port: '5432',
   user: 'sarvesh18',
   database: 'sarvesh18',
@@ -38,9 +37,9 @@ function hash(input, salt) {
 app.get('/hash/:input', function (req, res) {
     var hashedString = hash(req.params.input,'salt');       
     res.send(hashedString);
-
 });
 
+var pool = new Pool(config);
 //Post Request
 app.post('/signup', function (req, res) {
     var username = req.body.username;
@@ -129,6 +128,19 @@ app.get('/logout', function (req, res) {
 
 });
 
+app.get('/:test-db', function (req, res) {
+
+	//make a select request
+        //return a response with the results
+	pool.query('SELECT * FROM test',function(err, result) {
+         if(err) {
+          res.status(500).send(err.toString());
+         } else {
+          res.send(JSON.stringify(result));
+          res.send(JSON.stringify(result.rows));//Only Rows
+         }
+	});
+});
 /*
 var pool = new Pool(config);
 
@@ -237,22 +249,6 @@ var articleName=req.params.articleName;
 res.send(createTemplate(articles[articleName]));
 });
 
-
-var pool = new Pool(config);
-
-app.get('/:test-db', function (req, res) {
-
-	//make a select request
-        //return a response with the results
-	pool.query('SELECT * FROM test',function(err, result) {
-         if(err) {
-          res.status(500).send(err.toString());
-         } else {
-          res.send(JSON.stringify(result));
-          res.send(JSON.stringify(result.rows));//Only Rows
-         }
-	});
-});
 */
 ////////////////////////////////////////////////////////////////////////////////
 app.get('/ui/style.css', function (req, res) {
