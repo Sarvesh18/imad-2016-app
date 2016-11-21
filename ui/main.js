10e555452bbee04d622769d1c449d64fd47078b7
Sarvesh Singh <sarvesh.singh18@hotmail.com>
2016-11-21 21:45:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -73,7 +73,7 @@
         console.log(password);
         request.open('POST', '/create-user', true);
         request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({username: username, password: password}));  
+        request.send(JSON.stringify({"username": "username", "password": "password"}));  
         register.value = 'Registering...';
     
     };
