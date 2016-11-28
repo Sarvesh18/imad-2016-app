81fa539eb6be940a02691105892496d19d6a30ea
Sarvesh Singh <sarvesh.singh18@hotmail.com>
2016-11-29 04:54:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,7 +13,7 @@ function loadLogin () {
     request.send();
 }
 function loadLoggedInUser (username) {
-    var msg =`<h1>Welcome `+username+` !!!</h1>`
+    var msg =`<h1>Welcome `+username+` !!!</h1>`;
     //var msg = `<h1> Welcome <i>${username}</i> !!!</h1>
     //<a href="/logout">Logout</a>`;
     document.getElementById('myMsg').innerHTML = msg;
