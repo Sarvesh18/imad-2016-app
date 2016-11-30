8a5ede966adf14fdac7075dce59755c550c82bd2
Sarvesh Singh <sarvesh.singh18@hotmail.com>
2016-11-30 13:36:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,6 @@
+loadArticles();
 loadLogin();
+
 function loadLogin () {
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
@@ -121,3 +123,29 @@ function loadLoggedInUser (username) {
         request.send(JSON.stringify({"name": name, "email": email, "subject": subject, "likes": likes}));
         submit.value = 'Wait...';
     };
+////////////////////////////////////////////////////////////////////////////////    
+function loadArticles () {
+        // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            var articles = document.getElementById('articles');
+            if (request.status === 200) {
+                var content = '<ul>';
+                var articleData = JSON.parse(this.responseText);
+                for (var i=0; i< articleData.length; i++) {
+                    content += `<li>
+                    <a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
+                    (${articleData[i].date.split('T')[0]})</li>`;
+                }
+                content += "</ul>"
+                articles.innerHTML = content;
+            } else {
+                articles.innerHTML('Oops! Could not load all articles!')
+            }
+        }
+    };
+    
+    request.open('GET', 'http://sarvesh18.imad.hasura-app.io/get-articles', true);
+    request.send(null);
+}
